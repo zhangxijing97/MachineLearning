07c90fd29f973812ee86521c45999f0801def195
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-24 14:59:25 -0700

    Update

---
+++ RegressionAndClassification/Week1/CostFunction.py
@@ -0,0 +1,9 @@
+# Optional lab: Cost function
+
+import numpy as np
+# %matplotlib widget
+import matplotlib.pyplot as plt
+# from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+
+print(np.__version__)
