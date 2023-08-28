d0fd6dc731091983eae22513dfdf6585b79f853e
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-28 13:04:13 -0700

    Update py

--- RegressionAndClassification/Week2/MultipleLinearRegression.py
+++ RegressionAndClassification/Week2/MultipleLinearRegression.py
@@ -4,3 +4,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 print(np.__version__)
 plt.style.use('./deeplearning.mplstyle')
+
+# Problem Statement
+# Size (sqft)     Number of Bedrooms     Number of floors     Age of Home     Price (1000s dollars)
+# 2104	          5	                     1	                  45	          460
+# 1416	          3	                     2	                  40	          232
+# 852	          2	                     1	                  35	          178
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"y Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+
+def predict_single_loop(x, w, b):
+    """
+    single predict using linear regression
+
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters
+      b (scalar):  model parameter
+
+    Returns:
+      p (scalar):  prediction
+    """
+    n = x.shape[0]
+    p = 0
+    for i in range(n):
+        p_i = x[i] * w[i]
+        p = p + p_i
+    p = p + b
+    return p
+
+# get a row from our training data
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+# make a prediction
+f_wb = predict_single_loop(x_vec, w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
