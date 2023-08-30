8c91566223f63cd841c8e573bc2ce9d357fc5744
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-29 19:20:04 -0700

    Update

---
+++ RegressionAndClassification/Week2/FeatureScalingandLearningRate.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import  load_house_data, run_gradient_descent
+from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+from lab_utils_common import dlc
+np.set_printoptions(precision=2)
+plt.style.use('./deeplearning.mplstyle')
