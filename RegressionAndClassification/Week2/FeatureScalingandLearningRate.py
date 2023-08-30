109de0bf1b9fb14a75fcd7ff3b01c8ffe4be01ee
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-29 22:07:34 -0700

    Update

--- RegressionAndClassification/Week2/FeatureScalingandLearningRate.py
+++ RegressionAndClassification/Week2/FeatureScalingandLearningRate.py
@@ -1,11 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from lab_utils_multi import  load_house_data, run_gradient_descent
+# from lab_utils_multi import  load_house_data, run_gradient_descent
 # from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
 # from lab_utils_common import dlc
 np.set_printoptions(precision=2)
 plt.style.use('./deeplearning.mplstyle')
 
 # load the dataset
-# X_train, y_train = load_house_data()
-# X_features = ['size(sqft)','bedrooms','floors','age']
+def load_house_data():
+    data = np.loadtxt("./data/houses.txt", delimiter=',', skiprows=1)
+    X = data[:,:4]
+    y = data[:,4]
+    return X, y
+X_train, y_train = load_house_data()
+X_features = ['size(sqft)','bedrooms','floors','age']
+
+# view the dataset and its features by plotting each feature versus price
+fig,ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("Price (1000's)")
+plt.show()
