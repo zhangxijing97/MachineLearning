868dfe84b4891fe7503afede263f06855ab59884
Xijing Zhang <zhangxijing97@gmail.com>
2023-08-29 21:27:45 -0700

    Update Optional Lab: Feature scaling and learning rate

--- RegressionAndClassification/Week2/FeatureScalingandLearningRate.py
+++ RegressionAndClassification/Week2/FeatureScalingandLearningRate.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_multi import  load_house_data, run_gradient_descent
-from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
-from lab_utils_common import dlc
+# from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+# from lab_utils_common import dlc
 np.set_printoptions(precision=2)
 plt.style.use('./deeplearning.mplstyle')
+
+# load the dataset
+# X_train, y_train = load_house_data()
+# X_features = ['size(sqft)','bedrooms','floors','age']
